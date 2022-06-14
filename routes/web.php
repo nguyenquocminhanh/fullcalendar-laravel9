183beba0523714bdb047dfde51efa7be5f5cd63b
minh <minhhanh.nguyen001@umb.edu>
2022-06-14 11:37:46 -0400

    update url

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CalenderController;
 
-Route::get('calendar-event', [CalenderController::class, 'index']);
-Route::post('calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
+Route::get('/', [CalenderController::class, 'index']);
+Route::post('/calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
