d04a0509e65a160b3b64d3c452ac07635b672ffe
minh <minhhanh.nguyen001@umb.edu>
2022-06-13 22:24:49 -0400

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalenderController;
+
+Route::get('calendar-event', [CalenderController::class, 'index']);
+Route::post('calendar-crud-ajax', [CalenderController::class, 'calendarEvents']);
